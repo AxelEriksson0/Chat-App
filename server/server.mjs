ffe48f4075964214bb678372f2e3b2f0326cbe10
Axel Eriksson <axeleriksson0@gmail.com>
2019-12-18 13:31:54 +0100

    Server setup

---
+++ server/server.mjs
@@ -0,0 +1,28 @@
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import express from 'express'
+import http from 'http'
+import socket from 'socket.io'
+
+const app = express()
+const server = http.Server(app)
+const io = socket(server)
+const port = process.env.PORT || 8000
+
+app.use(cors())
+app.use(bodyParser.json())
+
+io.on('connection', socket => {
+  console.log(`Socket ID ${socket.id} connected`)
+  socket.on('message', message => {
+    console.log(`Socket ID ${socket.id} sent message`)
+  })
+
+  socket.on('disconnect', () => {
+    console.log(`Socket ID ${socket.id} left`)
+  })
+})
+
+server.listen(port, () => {
+  console.log(`Listening on port ${port}...`)
+})
