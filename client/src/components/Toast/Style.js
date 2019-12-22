e4b9e867c284a296e6ba6c4ffecf4de65207de23
Axel Eriksson <axeleriksson0@gmail.com>
2019-12-22 19:23:36 +0100

    Made Toast dynamic depending on type of message

--- client/src/components/Toast/Style.js
+++ client/src/components/Toast/Style.js
@@ -1,8 +1,19 @@
 import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles(theme => ({
-  close: {
-    padding: theme.spacing(0.5)
+  error: {
+    backgroundColor: theme.palette.error.dark
+  },
+  icon: {
+    fontSize: 20
+  },
+  iconVariant: {
+    opacity: 0.9,
+    marginRight: theme.spacing(1)
+  },
+  message: {
+    display: 'flex',
+    alignItems: 'center'
   }
 }))
 
--- client/src/components/Toast/Toast.js
+++ client/src/components/Toast/Toast.js
@@ -1,55 +1,63 @@
 import React from 'react'
-import Snackbar from '@material-ui/core/Snackbar'
-import IconButton from '@material-ui/core/IconButton'
-import CloseIcon from '@material-ui/icons/Close'
 import PropTypes from 'prop-types'
+
+import { IconButton, Snackbar, SnackbarContent } from '@material-ui/core'
+import CloseIcon from '@material-ui/icons/Close'
+import ErrorIcon from '@material-ui/icons/Error'
+import InfoIcon from '@material-ui/icons/Info'
 
 import useStyles from './Style'
 
+const iconVariant = {
+  error: ErrorIcon,
+  info: InfoIcon
+}
+
 const Toast = (props) => {
   const classes = useStyles()
+  const { message, open, setOpen, variant } = props
+  const Icon = iconVariant[variant]
 
   const handleClose = (event, reason) => {
-    if (reason === 'clickaway') {
-      return
-    }
-    props.setOpen(false)
+    if (reason === 'clickaway') { return }
+    setOpen(false)
   }
 
   return (
-    <div>
+    <>
       <Snackbar
-        anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'center'
-        }}
-        open={props.open}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        open={open}
         autoHideDuration={5000}
         onClose={handleClose}
-        ContentProps={{
-          'aria-describedby': props.message
-        }}
-        message={<span id="message-id">{props.message}</span>}
-        action={[
-          <IconButton
-            key="close"
-            aria-label="close"
-            color="inherit"
-            className={classes.close}
-            onClick={handleClose}
-          >
-            <CloseIcon />
-          </IconButton>
-        ]}
-      />
-    </div>
+        ContentProps={{ 'aria-describedby': message }}>
+
+        <SnackbarContent
+          className={`${classes[variant]}`}
+          aria-describedby="client-snackbar"
+          message={
+            <span id="client-snackbar" className={classes.message}>
+              <Icon className={`${classes.iconVariant} ${classes.icon}`} />
+              {message}
+            </span>
+          }
+          action={[
+            <IconButton key="close" aria-label="close" color="inherit" onClick={handleClose}>
+              <CloseIcon className={classes.icon} />
+            </IconButton>
+          ]}
+        />
+
+      </Snackbar>
+    </>
   )
 }
 
 Toast.propTypes = {
   message: PropTypes.string.isRequired,
   open: PropTypes.bool.isRequired,
-  setOpen: PropTypes.func.isRequired
+  setOpen: PropTypes.func.isRequired,
+  variant: PropTypes.string.isRequired
 }
 
 export default Toast
